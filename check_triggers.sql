84d67ecf2a02d3b678c9ef43148feeb125fe53ef
bkndhn <bknqwe19@gmail.com>
2026-09-02 19:40:13 +0530

    chore: remove temporary script files

--- check_triggers.sql
+++
@@ -1,3 +0,0 @@
-SELECT event_object_table, trigger_name
-FROM information_schema.triggers
-WHERE trigger_name LIKE '%push%' OR trigger_name LIKE '%fcm%';
--- extract.py
+++
@@ -1,9 +0,0 @@
-file_path = 'src/utils/imageUtils.ts'
-with open(file_path, 'r', encoding='utf-8') as f:
-    content = f.read()
-
-start = content.find("export const compressImage")
-if start != -1:
-    end = content.find("export const getCDNUrl", start)
-    with open('compressImage.txt', 'w', encoding='utf-8') as out:
-        out.write(content[start:end])
